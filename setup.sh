7f15a41427f00c6729934b63914c989004fa17c4
Daniel Weston <Weston.Daniel.A@gmail.com>
2021-11-18 20:25:28 +1000

    Init dotfile repo

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install required tools
+brew install tmux
+brew install wget
+brew install git
+brew install gh
+brew install nvm
+brew install yarn
+brew install terraform
+brew install docker
+
+# Not sure about these yet
+# brew install minikube
+
+# Install required apps
+brew install --cask alfred
+brew install --cask bettertouchtool
+brew install --cask rectangle
+brew install --cask appcleaner
+brew install --cask stats
+brew install --cask iterm2
+brew install --cask google-chrome
+brew install --cask messenger
+brew install --cask slack
+brew install --cask discord
+brew install --cask notion
+brew install --cask ticktick
+brew install --cask spotify
+brew install --cask github
+brew install --cask visual-studio-code
+brew install --cask postman
+
+# Has issues on Macbook 2021
+# brew install --cask docker
+
+# Only used for 2021 Macbook Pro
+# brew install --cask topnotch
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# Symlink dotfiles
+dotfiles="zshrc aliases p10k.zsh tmux.conf"
+dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for file in $dotfiles; do
+  ln -sfv $dotfiles_dir/$file ~/.$file
+done
